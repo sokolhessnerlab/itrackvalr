57b696a3c91026ed91f6878e842d6858ee063e1b
Ari Dyckovsky <aridyckovsky@gmail.com>
2021-04-26 20:17:57 -0600

    add extraction method basics for behavioral data
    renames with `raw_*` prefixes

--- _targets.R
+++ _targets.R
@@ -27,7 +27,7 @@ config <- config::get()
 
 # Begin attempt at mat file extraction direct from R for targets pipeline
 # Potential extractor method for v5 MAT files: https://stackoverflow.com/questions/58418852/how-to-import-matlab-table-cell-structure-to-r-dataframe-while-conserving-pr
-extract_edf_mat <- function(mat_file) {
+extract_edf_data <- function(mat_file) {
   mat_data <- readMat(mat_file)
   struct <- mat_data$Edf2Mat
   struct_data <- struct[,,1]
@@ -47,14 +47,26 @@ extract_edf_mat <- function(mat_file) {
   #sample_df <- as.data.frame(t(sample[,,]))
 }
 
+extract_behavioral_data <- function(mat_file) {
+  mat_data <- readMat(mat_file)
+  struct <- mat_data$subjdata
+  struct_data <- struct[,,1]
+  # return a df
+}
+
 # Configure pipeline
 list (
   tar_files(
-    eyetracker_mat_files,
+    raw_eyetracker_mat_files,
     list.files(here::here(config$path$data, "raw", "eyetracker", "responses"), full.names = TRUE)
   ),
   tar_files(
-    behavioral_mat_files,
+    raw_behavioral_mat_files,
     list.files(here::here(config$path$data, "raw", "behavioral", "responses"), full.names = TRUE)
+  ),
+  tar_target(
+    extracted_behavioral_data,
+    extract_behavioral_data(raw_behavioral_mat_files),
+    pattern = map(raw_behavioral_mat_files)
   )
 )
