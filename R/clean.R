47389e6020310d3ca97f31b7cd7295ac7ce917cd
Ari Dyckovsky <aridyckovsky@gmail.com>
2021-05-18 18:22:07 -0600

    add namespace to str_ functions

--- R/clean.R
+++ R/clean.R
@@ -8,7 +8,7 @@
 #' @export
 clean_behavioral_data <- function(csv_file, id_prefix = "CSN") {
   # Get the padded 3-digit id from the file name, including a prefix
-  padded_id <- str_extract(csv_file, str_c(id_prefix,"\\d{3}"))
+  padded_id <- stringr::str_extract(csv_file, str_c(id_prefix,"\\d{3}"))
 
   df <- read_csv(csv_file)
 
@@ -17,7 +17,7 @@ clean_behavioral_data <- function(csv_file, id_prefix = "CSN") {
    # mutate(is_double_tick = step_type > 1) # mutate step type to a boolean for double tick
 
   # Construct output file
-  output_file <- str_c(here::here(config$path$data, "clean", "behavioral", padded_id), ".csv")
+  output_file <- stringr::str_c(here::here(config$path$data, "clean", "behavioral", padded_id), ".csv")
 
   # Write dataframe to output file
   write_csv(df, output_file)
