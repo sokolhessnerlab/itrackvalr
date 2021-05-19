67964a76fa31095a8254bdf299dad27b7e9b69cb
Ari Dyckovsky <aridyckovsky@gmail.com>
2021-05-19 10:55:05 -0600

    refactor: use modular lists for nested target steps
    Moves toward a more robust, scalable pipeline infrastructure, including the use of `tar_combine` for mapped target objects.

--- _targets.R
+++ _targets.R
@@ -37,34 +37,40 @@ participants <- tibble(
     purrr::map(~ stringr::str_extract(.x, "CSN\\d{3}"))
 )
 
-list(
-  # Per participant mapping of targets using static branching for better manifest output
-  tar_map(
-    values = participants,
-
-    # Set raw behavioral file targets
-    # TODO: change the `str_subset(...` to a function!
-    tar_target(
-      raw_behavioral_file,
-      str_subset(list.files(here::here(config$path$data, "raw", "behavioral", "responses"), full.names = TRUE), id),
-      format = "file"
-    ),
-
-    # Set raw eyetracker file targets
-    # TODO: change the `str_subset(...` to a function!
-    tar_target(
-      raw_eyetracker_file,
-      str_subset(list.files(here::here(config$path$data, "raw", "eyetracker", "responses"), full.names = TRUE), id),
-      format = "file"
-    ),
-
-    # Extract behavitoral data from raw MAT and output dataframe
-    # Side effect: Writes dataframe as CSV to `data/extracted`
-    tar_target(
-      extracted_behavioral_data,
-      extract_behavioral_data(raw_behavioral_file)
-    )
+mapped_extraction <- tar_map(
+  #unlist = FALSE, # Return a nested list from tar_map()
+  values = participants,
+  # Set raw behavioral file targets
+  # TODO: change the `str_subset(...` to a function!
+  tar_target(
+    raw_behavioral_file,
+    str_subset(list.files(here::here(config$path$data, "raw", "behavioral", "responses"), full.names = TRUE), id),
+    format = "file"
   ),
+  # Set raw eyetracker file targets
+  # TODO: change the `str_subset(...` to a function!
+  tar_target(
+    raw_eyetracker_file,
+    str_subset(list.files(here::here(config$path$data, "raw", "eyetracker", "responses"), full.names = TRUE), id),
+    format = "file"
+  ),
+  # Extract behavitoral data from raw MAT and output dataframe
+  # Side effect: Writes dataframe as CSV to `data/extracted`
+  tar_target(
+    extracted_behavioral_data,
+    extract_behavioral_data(raw_behavioral_file)
+  )
+)
+
+combined_behavioral <- tar_combine(
+  extracted_behavioral_data_combined,
+  mapped_extraction$extracted_behavioral_data,
+  command = dplyr::bind_rows(!!!.x)
+)
+
+list(
+  mapped_extraction,
+  combined_behavioral,
   tar_render(
     behavioral_data_preprocessing_notebook,
     "notebooks/behavioral_data_preprocessing.Rmd"
